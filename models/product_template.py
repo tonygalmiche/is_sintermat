3afaf257b60b5942b12dd7c8c85eb36691092b49
Tony Galmiche <tony.galmiche@infosaone.com>
2019-07-04 15:27:11 +0200

    modifs

--- models/product_template.py
+++ models/product_template.py
@@ -7,3 +7,29 @@ class ProductTemplate(models.Model):
 
     is_dimensions = fields.Char(u"Dimensions")
     is_matiere    = fields.Char(u"Matière")
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for obj in self:
+            name=u''
+            if obj.default_code:
+                name+=u'['+obj.default_code+u']'
+            if obj.is_dimensions:
+                name+=u'['+obj.is_dimensions+u']'
+            if obj.is_matiere:
+                name+=u'['+obj.is_matiere+u']'
+            if name!=u'':
+                name+=u' '
+            name=name+obj.name
+            result.append((obj.id, name))
+        return result
+
+
+
+
+
